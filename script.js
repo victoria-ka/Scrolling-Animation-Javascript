cd4ca0a5d940889af13001c2d49c9307f0ce0d48
Victoria <victoria.austin1997@gmail.com>
2019-05-26 22:46:38 -0400

    Delete random underscores

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Easing function used for `translateX` animation
 // From: https://gist.github.com/gre/1650294
 function easeOutQuad (t) {
-    return t *_ (2 - t);
+    return t * (2 - t);
 }
   
 // Returns a random number (integer) between __`min`__ and __`max`__
 function random (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min_;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   
 // Returns a random number as well, but it could be negative also
